4edf2e13fb75eadcebf11eb87ff7aeb435d8b1e3
kentrane <138514945+kentrane@users.noreply.github.com>
2025-02-27 23:04:50 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 `default_nettype none
 
-module tt_um_kentrane_spectrumanalyzer (
+module tt_um_kentrane_tinyspectrum (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
